30d17454f4d6a77666054b957afa51856d36f22c
Yead191 <yead191@gmail.com>
2025-06-03 15:21:51 +0600

    fix deploy issue

--- 9am-task-client/src/main.jsx
+++ 9am-task-client/src/main.jsx
@@ -1,33 +1,29 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { RouterProvider, createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router";
 import router from "./routes/routes.jsx";
 import { Toaster } from "sonner";
 import ShopDashboard from "./pages/DashboardPages/ShopDashboard";
 
 // Determine subdomain
-const hostnameParts = window.location.hostname.split(".");
-const subdomain =
-  hostnameParts.length >= 3 && hostnameParts[0] !== "localhost"
-    ? hostnameParts[0]
-    : null;
+// const hostnameParts = window.location.hostname.split(".");
+// const subdomain =
+//   hostnameParts.length >= 3 && hostnameParts[0] !== "localhost"
+//     ? hostnameParts[0]
+//     : null;
 
 // Create a fallback router for the ShopDashboard if needed
-const shopRouter = createBrowserRouter([
-  {
-    path: "/",
-    element: <ShopDashboard shopName={subdomain} />,
-  },
-]);
+// const shopRouter = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <ShopDashboard shopName={subdomain} />,
+//   },
+// ]);
 
 createRoot(document.getElementById("root")).render(
   <>
     <Toaster position="top-right" />
-    {subdomain ? (
-      <RouterProvider router={shopRouter} />
-    ) : (
-      <RouterProvider router={router} />
-    )}
+    <RouterProvider router={router} />
   </>
 );
